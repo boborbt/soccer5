c3be8092e25154095b02c31aea2048ae9aaaa1e6
Roberto Esposito <esposito@di.unito.it>
2009-03-07 17:59:15 +0000

    [NEW] support for editing and showing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController  
-  skip_before_filter :login_required
+  skip_before_filter :login_required, :only => [:create, :new]
   
   # render new.rhtml
   def new
@@ -19,6 +19,45 @@ class UsersController < ApplicationController
       flash[:notice] = "Thanks for signing up!"
     else
       render :action => 'new'
+    end
+  end
+  
+  def index
+    @users = User.find(:all)
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+  
+  def show
+     @user = User.find(params[:id])
+
+     respond_to do |format|
+       format.html # show.html.erb
+       format.xml  { render :xml => @user }
+     end
+   end
+  
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.role = Role.find(params[:user][:role])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
     end
   end
 
