e91ecead93c2fc0b43dd7b873c3fbe5c007c4ebb
Roberto Esposito <esposito@di.unito.it>
2009-04-01 17:22:42 +0000

    [NEW] created SH file for running all tasks that needs to be done hourly

---
+++ script/hourly.sh
@@ -0,0 +1,5 @@
+RAILS_ENV=production /srv/www/htdocs/calcetto/current/script/runner /srv/www/htdocs/calcetto/current/script/calcetto/autoinvite.rb
+
+RAILS_ENV=production /srv/www/htdocs/calcetto/current/script/runner /srv/www/htdocs/calcetto/current/script/calcetto/close_match.rb
+
+RAILS_ENV=production /srv/www/htdocs/calcetto/current/script/runner /srv/www/htdocs/calcetto/current/script/calcetto/solicitation.rb
