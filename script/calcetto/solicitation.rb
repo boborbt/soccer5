b506bee5a873c92238076dcd9cb689d6ce7a9cf9
Roberto Esposito <esposito@di.unito.it>
2009-04-03 07:59:33 +0000

    [FIX] Changed timings for solicitations (now they are 4 and 2 days)

--- script/calcetto/solicitation.rb
+++ script/calcetto/solicitation.rb
@@ -3,12 +3,12 @@ logger = RAILS_DEFAULT_LOGGER
 logger.info('SOLICITATION-SCRIPT:: Analyzing matches (possibly) needing to be solicited...')
 
 Match.all_waiting_matches.each do |match|
-  if ((match.date - Date.today).days < 3.days) && match.status==Match::STATUSES[:waiting]    
+  if ((match.date - Date.today).days < 5.days) && match.status==Match::STATUSES[:waiting]    
     logger.info("SOLICITATION-SCRIPT:: Sending first solicitation to match #{match.description}")
     match.solicit_players!
   end
   
-  if ((match.date - Date.today).days < 2.days) && match.status==Match::STATUSES[:solicit1]
+  if ((match.date - Date.today).days < 3.days) && match.status==Match::STATUSES[:solicit1]
     logger.info("SOLICITATION-SCRIPT:: Sending second solicitation to match #{match.description}")
     match.solicit_players!
   end
