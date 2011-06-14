4920d837dd1bbb90abfb1310d0179fd009ae7afd
Roberto Esposito <boborbt@gmail.com>
2011-06-14 17:59:13 +0200

    Improved spinning wheel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def spinner_box
+    %Q{<div id="spinner" style="display:none">
+    	<div class="content">#{image_tag 'spinner_big.gif'}</div>
+    </div>}
+  end
+  
+  def show_spinner
+    %q{$('spinner').appear( {duration:0.5})}
+  end
 end
